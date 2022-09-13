d46f831d9a8cab8800a445fbb9d6cc47f6d81e7f
RodericoNeto <talysonsoares40@gmail.com>
2022-09-13 20:58:38 -0300

    adicionou routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/login'>Login</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <h1>Book Favorites</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<h1>Não tem essa página :(</h1>} />
+        </Routes>
+      </BrowserRouter>
+
+      <Footer />
+    </>
   );
 }
 
